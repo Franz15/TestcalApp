6ad34047a4558f9a67854997f6e60fec53e3b0b4
Franz15 <vallinas15@gmail.com>
2024-06-25 23:53:36 +0200

    Cambios en el layout Dashboard y en la visualización de los charts

--- client/src/components/layout/private/Dashboard.jsx
+++ client/src/components/layout/private/Dashboard.jsx
@@ -1,16 +1,11 @@
-import React, {useState, useEffect} from "react";
-import { Box, Card, Grid } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import { Table9c } from "../../accesories/Table9c";
 import { Global } from "../../../helpers/Global";
-import { Table9c } from "../../accesories/Table9c";
 import Ratings from "../../accesories/Ratings";
 import { RadarChart } from "../../accesories/RadarChart";
 import { LinearChart } from "../../accesories/LinearChart";
-import Note from "../../accesories/Note";
 import { useAuth } from "../../../hooks/useAuth";
 import "./dashboard.css";
-import Alert from "@mui/material/Alert";
-import getLastResult from "../../../hooks/test9c/getLastResult";
 
 export default function Dashboard() {
   //Token de autenticación
@@ -79,16 +74,19 @@ export default function Dashboard() {
             <RadarChart results={results} />
           </div>
         </article>
-
+        <article className="radar-chart">
+          <div>
+            <RadarChart results={results} />
+          </div>
+        </article>
         <article className="table">
-          <Table9c results={results} handleResults={handleResults} />
+        <LinearChart results={results} />
         </article>
 
         <article className="linear-chart">
-          <LinearChart results={results} />
-        </Grid>
-      </Grid>
-      <Note />
-    </Box>
+        <Table9c results={results} handleResults={handleResults} />
+        </article>
+      </section>
+    </>
   );
 }
